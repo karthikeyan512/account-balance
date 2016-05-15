9a19edf1e6faf1b480f2b7a31afbbd21a776a560
Karthikeyan <karthikeyan512@gmail.com>
2016-05-15 22:51:59 +0800

    Initial commit

---
+++ AccountBalance/src/main/java/com/karthik/AccountBalanceApplication.java
@@ -0,0 +1,12 @@
+package com.karthik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountBalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountBalanceApplication.class, args);
+	}
+}
